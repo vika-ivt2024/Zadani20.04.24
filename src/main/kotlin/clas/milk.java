97f7ed61b88ffca632931de00fa156f363347d9b
Vika Chudnovskai <viktoria.chudnovskay@yandex.ru>
2024-04-26 22:38:36 +0300

    secont commin

---
+++ src/main/kotlin/clas/milk.java
@@ -0,0 +1,11 @@
+package clas;
+
+public class milk {
+    val name = "концерн «Покровский"
+    val kategori = "Нормализованное"
+    val cena = 150
+    val kolichestvo = 30
+    val stopimostEGG = cena*kolichestvo
+    println("Характеристики молоко: /n")
+    println("Название: $name, Категория: $kategori, Цена: $cena , Количество на складе: $kolichestvo")
+}
