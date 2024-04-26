c16658de4175a0a568e90ad3d65159cc716d7c06
Vika Chudnovskai <viktoria.chudnovskay@yandex.ru>
2024-04-26 22:38:36 +0300

    3 commin

---
+++ src/main/kotlin/clas/hleb.java
@@ -0,0 +1,11 @@
+package clas;
+
+public class hleb {
+    val name = "Хлебопек"
+    val kategori = "Коврига"
+    val cena = 20
+    val kolichestvo = 15
+    val stopimostEGG = cena*kolichestvo
+    println("Характеристики хлеб: /n")
+    println("Название: $name, Категория: $kategori, Цена: $cena , Количество на складе: $kolichestvo")
+}
